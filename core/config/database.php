c3bb7e04f561be0b65e6ca93590f2f9eaf42bfcc
Tania <oliaritianatania23@gmail.com>
2021-02-16 14:39:44 +0100

    redme.md

--- core/config/database.php
+++ core/config/database.php
@@ -2,8 +2,8 @@
 
 $this->config['db'] = array(
 	'driver'   => 'mysql',
-	'host'     => 'http://medialibsos-002.medialibs.com/mysql',
-	'username' => 'testmas189265com',
-	'password' => 'rGWJwauZ',
-	'name'     => 'dbtestmaerys189265com'
+	'host'     => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'name'     => 'annuaire-management'
 );
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Requirements
+- php version 7.3
+
+# To run the app
+- php -S localhost:8080 
+
