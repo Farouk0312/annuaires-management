ea479e178f06a9b75ca87c7ccfb4c653fddf08f7
Tania <oliaritianatania23@gmail.com>
2021-02-16 14:26:49 +0100

    Update database.php

--- core/config/database.php
+++ core/config/database.php
@@ -2,7 +2,7 @@
 
 $this->config['db'] = array(
 	'driver'   => 'mysql',
-	'host'     => 'medialibsos-002.medialibs.com/mysql',
+	'host'     => 'http://medialibsos-002.medialibs.com/mysql',
 	'username' => 'testmas189265com',
 	'password' => 'rGWJwauZ',
 	'name'     => 'dbtestmaerys189265com'
